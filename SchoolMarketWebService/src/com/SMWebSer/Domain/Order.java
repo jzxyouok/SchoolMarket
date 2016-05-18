0df7fa714a14279a5611f6842d09f5dc4dad29f8
HaloRain <540924871@qq.com>
2016-05-18 22:37:48 +0800

    Order.java
    Order class

---
+++ SchoolMarketWebService/src/com/SMWebSer/Domain/Order.java
@@ -0,0 +1,53 @@
+package com.SMWebSer.Domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "order")
+
+public class Order {
+	@Id
+	@Column(name = "order_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	private Integer id;
+	private String name;
+	private String time;
+	private Integer totalPrice;
+	
+	public Order(){}	
+	
+	
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public Integer getTotalPrice() {
+		return totalPrice;
+	}
+	public void setTotalPrice(Integer totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+}
